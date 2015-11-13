5874dd08d2b986f63c164509747fdd45cce608c9
Arnold <arnoldsander@gmail.com>
2015-11-13 10:59:51 -0500

    Mongo Crud to Blaze templates addition

--- simple-todos.js
+++ simple-todos.js
@@ -1,11 +1,35 @@
+Tasks = new Mongo.Collection("tasks");
+ 
 if (Meteor.isClient) {
+  // This code only runs on the client
   Template.body.helpers({
-    tasks: [{text: "This is task 1"}, 
-            {text: "This is task 2"}
-    ]
-    
+    tasks: function () {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
   });
 
+  Template.body.events({
+    "submit .new-task": function (event){
+      event.preventDefault(); // Disable normal form submit
+
+      var text = event.target.text.value; // get and Store form value
+
+      Tasks.insert({ text: text, createdAt: new Date()}); //Create a new record for Mongo DB collection
+
+      event.target.text.value = "";
+    }
+  });
+  Template.task.events({
+    "click .toggle-checked": function() {
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function (){
+      Tasks.remove(this._id);
+    }
+
+  });
 }
 
 if (Meteor.isServer) {
